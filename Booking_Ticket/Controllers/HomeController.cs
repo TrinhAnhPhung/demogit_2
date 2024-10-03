3978553d53dc22a7e08be648eedf9a6196220057
Trinhanhphung <anhphungtrinh2005@gmail.com>
2024-10-03 17:07:38 +0700

    Thêm nội dung

--- Booking_Ticket/Controllers/HomeController.cs
+++ Booking_Ticket/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace Booking_Ticket.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult HomePage1()
+        {
+            return View();
+        }
         public ActionResult HomePage()
         {
             return View();

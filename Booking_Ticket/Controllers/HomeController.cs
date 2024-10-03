2671e435dfa3b1e39c8f99983f00b5b07279a6c7
Trinhanhphung <Trinhanhphung@DESKTOP-9QSEI6H>
2024-10-03 14:14:31 +0700

    Add project files.

---
+++ Booking_Ticket/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Booking_Ticket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Booking_Ticket/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Booking_Ticket.MvcApplication" Language="C#" %>
